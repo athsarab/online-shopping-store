58884c8bf020bab9b0cb081866ea6e6bfea9a33e
athsara bimalka <152287082+athsarab@users.noreply.github.com>
2026-01-20 13:46:59 +0530

    Fix HTML encoding issues in Men.php

--- PHP/Men.php
+++ PHP/Men.php
@@ -7,7 +7,7 @@
     <title>Men</title>
     <!--link css-->
     <link rel="stylesheet" href="../ASS/CSS/men.css">
-    <!--box icons-->
+    <!--box icons--> 
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <?php include "header.php"; ?>
@@ -159,6 +159,7 @@
 
 
 </html>
+
 
 
 
